9f50ba5b9304d5abfbc08c477c5c011a2cba1759
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-11 18:13:45 +0530

    Create Area.java

---
+++ Area.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ public class Area
+{
+	public static void main (String[] args) 
+	{
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=((a*b)/2);
+		System.out.println(c);
+	}
+}
