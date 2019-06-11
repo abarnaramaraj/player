d262e9f3fc5dc6f4f8268cc85a86d3ee13fe6d56
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-11 19:47:26 +0530

    Create Number.java

---
+++ Number.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Number
+{
+	public static void main (String[] args) 
+	{
+		int a,b,x,y;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		x=sc.nextInt();
+		y=((a*x)+b);
+		System.out.println(y);
+	}
+}
