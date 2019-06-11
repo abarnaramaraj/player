59ac4083aafc4813c4b8aa43a4e5ffc4d9646a01
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-11 20:22:37 +0530

    Create Summ.java

---
+++ Summ.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Summ
+{
+	public static void main (String[] args) 
+	{
+		int a;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		if(a%3==0)
+		{
+		System.out.println("yes");
+		}
+		else 
+		System.out.println("no");
+		
+	}
+}
