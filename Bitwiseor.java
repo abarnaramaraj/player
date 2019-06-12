d192a68b2d8b9ffcbcfbd0ca84dc973091a59265
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-12 14:32:53 +0530

    Create Bitwiseor.java

---
+++ Bitwiseor.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ public class Bitwiseor
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=a|b;
+		System.out.println(c);
+		
+	}
