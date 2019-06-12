3ed09a28d529b589b6fcbbd9746dfb3e53a8785b
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-12 15:32:51 +0530

    Create Bitwiseleft.java

---
+++ Bitwiseleft.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Bitwiseleft
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=(a<<b);
+		System.out.println(c);
+		
+	}
+}
