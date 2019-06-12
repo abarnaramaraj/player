1c66d082b53c6d74d24f130a2b6f3df4beced5bf
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-12 14:40:10 +0530

    Create Bitwisenot.java

---
+++ Bitwisenot.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Bitwisenot
+{
+	public static void main (String[] args) 
+	{
+		int n,c;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		c=~n;
+		System.out.println(c);
+		
+	}
+}
