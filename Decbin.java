2d1f9ecfbf3c38d6f7e50ddcb369569d6fd32465
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-13 09:58:43 +0530

    Create Decbin.java

---
+++ Decbin.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ public class Decbin
+{
+	public static void main (String[] args) 
+	{
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n= sc.nextInt();
+		System.out.println(Integer.toBinaryString(n));
+	}
+}
