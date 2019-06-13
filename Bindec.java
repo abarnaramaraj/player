9b93e51a9b670382e2546054551c3ce13e999746
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-13 10:11:11 +0530

    Create Bindec.java

---
+++ Bindec.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+ public class Bindec
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		 String n= sc.nextLine();
+		System.out.println(Integer.parseInt(n,2));
+	}
+}
