9dc4931640cfa637fb9befcaf72fe993426f6995
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-19 13:27:56 +0530

    Create Upperlower.java

---
+++ Upperlower.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Upperlower
+ {
+  
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    String s=in.nextLine();
+    String si=s.toUpperCase();
+    String vi=s.toLowerCase();
+
+    for(int i=0;i<s.length();i++)
+{
+      if(s.charAt(i)>='a'&&s.charAt(i)<='z')
+{
+        System.out.print(si.charAt(i));
+      }
+      else
+{
+        System.out.print(vi.charAt(i));
+      }
+    }
+  }
+  
+}
