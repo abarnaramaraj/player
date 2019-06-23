73dcaae8c10572cce5322acff4c7b8ce012d14eb
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-23 15:14:21 +0530

    Update and rename Primeinterval.java to Primeinterval1.java

--- Primeinterval1.java
+++ Primeinterval1.java
@@ -1,23 +1,23 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-public claas Primeinterval
+public claas Primeinterval1
 {
 	public static void main (String[] args) 
 	{
-		int f,u ,g=1,h=0;
+		int f,u ,g1=1,h=0;
         Scanner sc=new Scanner (System.in);
         f=sc.nextInt();
         u=sc.nextInt();
 		for(int j=f;j<=u;j++)
 		{
-			g=0;
+			g1=0;
 			for(int k=1;k<=j;k++)
 			{
 				if(j%k==0)
-					g++;
+					g1++;
 			}
-			if(g==2)
+			if(g1==2)
 				h++;
 		}
 		System.out.println(h);
